9f4c71dcbdd12d2fae550c5e822aea674860c267
spikeedjet <spike.edjet@gmail.com>
2022-04-29 14:46:58 +0800

    add scaffold post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_031231) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_064107) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
