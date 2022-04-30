71b624fe6a4f0dfb6ddb59d6d40474a1f859e57e
spikeedjet <spike.edjet@gmail.com>
2022-04-30 20:20:03 +0800

    beautify UI

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
 	validates :content, length: { in: 0..200 }
 	has_rich_text :content 
 	belongs_to :user
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end
--- db/migrate/20220429131201_add_userid_to_post.rb
+++
@@ -1,6 +0,0 @@
-class AddUseridToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :user_id, :integer
-    add_index :posts, :user_id
-  end
-end
