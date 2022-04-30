f67ed5ab529b204dd5a609ba2075f35439b17fda
spikeedjet <spike.edjet@gmail.com>
2022-04-30 21:49:50 +0800

    beautify posts and my posts ui

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-	validates :title, :content, presence: true
-	validates :title, length: { in: 0..50 }
-	validates :content, length: { in: 0..200 }
+	validates :title, presence: true
+	validates :title, length: { in: 0..200 }
+	validates :content, length: { in: 0..1000 }
 	has_rich_text :content 
 	belongs_to :user
 	has_many :comments, dependent: :destroy
